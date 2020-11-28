f2181b2251c47498f01401f1b4a27df4c3b803c2
Pc89752 <91a04b08@gmail.com>
2020-11-28 20:41:25 +0800

    Console
    Add Console and change README.md

---
+++ BlackJack/Console.java
@@ -0,0 +1,51 @@
+package BlackJack;
+
+import java.util.Scanner;
+
+public class Console {
+    Scanner sc=new Scanner(System.in);
+
+    public int getInt(String str) {
+        while (true) {
+            try {
+                System.out.print(str);
+                int input = sc.nextInt();
+                sc.nextLine();
+                return input;
+            } catch (Exception e) {
+                sc.nextLine();
+                System.out.println("Please enter correctly");
+            }
+        }
+    }
+
+    public String getString(String[] strs) {
+        while (true) {
+            String input = sc.nextLine().toUpperCase();
+            for (String string : strs) {
+                if(input.equals(string)){
+                    return input;
+                }
+            }
+            System.out.println("Please enter correctly");
+        }
+    }
+
+    public String getString() {
+        return sc.nextLine();
+    }
+
+    public double getDouble(String str){
+        while (true) {
+            try {
+                System.out.print(str);
+                double input = sc.nextDouble();
+                sc.nextLine();
+                return input;
+            } catch (Exception e) {
+                sc.nextLine();
+                System.out.println("Please enter correctly");
+            }
+        }
+    }
+}
